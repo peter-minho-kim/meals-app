561ff9378c27bf2eaa533ae2d442c140b6a5112d
Peter Kim <peter.minho.kim@gmail.com>
2020-12-05 11:39:51 -0500

    Adds tabs-based navigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,9 +1,11 @@
 import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
 import Colors from '../constants/Colors';
 
 const MealsNavigator = createStackNavigator(
@@ -29,4 +31,9 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator({
+  Meals: MealsNavigator,
+  Favorites: FavoritesScreen
+});
+
+export default createAppContainer(MealsFavTabNavigator);
